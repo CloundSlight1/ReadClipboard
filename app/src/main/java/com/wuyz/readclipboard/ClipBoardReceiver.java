d84bb93fc1a664f6198053e75a8cb0d27f0b2aaa
wuyuzhong <wuyuz@landicorp.com>
2017-01-03 13:47:08 +0800

    dev

--- app/src/main/java/com/wuyz/readclipboard/ClipBoardReceiver.java
+++ app/src/main/java/com/wuyz/readclipboard/ClipBoardReceiver.java
@@ -7,9 +7,12 @@ import android.content.Context;
 import android.content.Intent;
 import android.text.TextUtils;
 import android.util.Log;
+import android.widget.Toast;
 
 /**
  * Created by wuyz on 2016/11/29.
+ * ClipBoardReceiver
+ * adb shell am broadcast -a readclipboard.action.SEND_TEXT --es key "value"
  */
 
 public class ClipBoardReceiver extends BroadcastReceiver {
@@ -19,8 +22,9 @@ public class ClipBoardReceiver extends BroadcastReceiver {
         Log.d("readclipboard", "value: " + value);
         if (!TextUtils.isEmpty(value)) {
             ClipboardManager manager = (ClipboardManager) context.getSystemService(Context.CLIPBOARD_SERVICE);
-            ClipData clipData = ClipData.newPlainText("wuyz", value);
+            ClipData clipData = ClipData.newPlainText("readclipboard", value);
             manager.setPrimaryClip(clipData);
+            Toast.makeText(context, value, Toast.LENGTH_LONG).show();
         }
     }
 }
